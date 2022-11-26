8c267571eeb5370c5e12b3d30cfbe3001e059334
harutpepanyan.0711@gmail.comcdgit reset Headgit config -- global user.email harutpepanyan.0711@gmail.com <harutpepanyan.0711@gmail.comgit config --global user.name Harutyun>
2022-11-26 13:02:42 +0400

    HP

--- js/users.js
+++ js/users.js
@@ -1,11 +1,6 @@
 const getUsers = async () => {
     console.log("Loading...");
-  
-    usersRequest().then(dataHandler).finally(console.log("Users are loaded"))
-  
-    
-  
-    console.log("users are loaded");
+  usersRequest().then(dataHandler).finally(console.log("Users are loaded"));
   };
   function dataHandler(data) {
     console.log(data);f
