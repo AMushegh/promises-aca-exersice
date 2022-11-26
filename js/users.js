80c8d06f5cfe1a524f7bc30fba57f72e21aeee0d
Panda223 <arakelimp@gmail.com>
2022-11-26 12:54:19 +0400

    add async version

--- js/users.js
+++ js/users.js
@@ -1,8 +1,16 @@
-const getUsers = () => {
+// const getUsers = () => {
+//     // your code goes here
+//     console.log("Loading ...");
+//     usersRequest().then((result) => console.log(JSON.parse(result)));
+//     console.log("Users are loaded");
+// };
+
+
+async function getUsers(){
     // your code goes here
     console.log("Loading ...");
-    usersRequest().then((result) => console.log(JSON.parse(result)));
+     
+    let promise = await usersRequest();
+    console.log(JSON.parse(promise));
     console.log("Users are loaded");
 };
-
-getUsers();
