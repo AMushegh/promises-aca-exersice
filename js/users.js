fe325b6db425889332089e6ca1e2d88fd1001688
Panda223 <arakelimp@gmail.com>
2022-11-26 12:57:26 +0400

    Add correct version with then

--- js/users.js
+++ js/users.js
@@ -1,16 +1,16 @@
-// const getUsers = () => {
-//     // your code goes here
-//     console.log("Loading ...");
-//     usersRequest().then((result) => console.log(JSON.parse(result)));
-//     console.log("Users are loaded");
-// };
-
-
-async function getUsers(){
+const getUsers = () => {
     // your code goes here
     console.log("Loading ...");
-     
-    let promise = await usersRequest();
-    console.log(JSON.parse(promise));
-    console.log("Users are loaded");
+    usersRequest().then((result) => console.log(JSON.parse(result))).finally(()=> console.log("Users are loaded"));
+    
 };
+
+
+// async function getUsers(){
+//     // your code goes here
+//     console.log("Loading ...");
+     
+//     let promise = await usersRequest();
+//     console.log(JSON.parse(promise));
+//     console.log("Users are loaded");
+// };
