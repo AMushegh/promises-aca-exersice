f7777e3a69014ba7f797b7e94e3fa715787c70c0
harutpepanyan.0711@gmail.comcdgit reset Headgit config -- global user.email harutpepanyan.0711@gmail.com <harutpepanyan.0711@gmail.comgit config --global user.name Harutyun>
2022-11-26 12:28:14 +0400

    HP

--- js/users.api.js
+++ js/users.api.js
@@ -132,7 +132,9 @@ let usersDataJSON = `[
 ]`;
 
 const usersPromise = new Promise((resolve, reject) => {
-    // your code goes here
+    setTimeout(() => {
+        resolve(usersDataJSON)
+    }, Math.floor(Math.random() * 10000) + 1)
 });
 
 const usersRequest = () => {
--- js/users.js
+++ js/users.js
@@ -1,3 +1,10 @@
-const getUsers = () => {
-    // your code goes here
-};
+const getUsers = async () => {
+    console.log("Loading...");
+  
+    const response = await usersRequest();
+    const data = await JSON.parse(response);
+  
+    console.log(data);
+  
+    console.log("users are loaded");
+  };
